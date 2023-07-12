9c0d975193d1abcda869d81eb367d582e06745f9
takker99 <37929109+takker99@users.noreply.github.com>
2023-07-13 08:38:56 +0900

    :arrow_up:

--- deps/preact.tsx
+++ deps/preact.tsx
@@ -1,8 +1,8 @@
-export * from "https://esm.sh/preact@10.5.15";
+export * from "https://esm.sh/preact@10.16.0";
 export {
   useCallback,
   useEffect,
   useMemo,
   useRef,
   useState,
-} from "https://esm.sh/preact@10.5.15/hooks";
+} from "https://esm.sh/preact@10.16.0/hooks";
