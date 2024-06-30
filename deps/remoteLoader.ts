601989d89e37cd2e2d228dfce2a3a47868a9dd32
takker99 <37929109+takker99@users.noreply.github.com>
2024-06-30 23:07:21 +0900

    fix: externalがあるときesbuildに不正なpropertyを渡していた

--- deps/remoteLoader.ts
+++ deps/remoteLoader.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.6.1/remoteLoader.ts";
+export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.6.2/remoteLoader.ts";
