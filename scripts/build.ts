306a9989f9cb52bb99d3160652cb05d0e52a9503
takker99 <37929109+takker99@users.noreply.github.com>
2024-06-30 20:57:03 +0900

    build: Use esbuild to build JS files

---
+++ scripts/build.ts
@@ -0,0 +1,24 @@
+import { build, denoPlugins, stop } from "../deps/esbuild.ts";
+
+const result = await build({
+    entryPoints: [
+        new URL("../App.tsx", import.meta.url).href,
+        new URL("../deps/worker.ts", import.meta.url).href,
+    ],
+    bundle: true,
+    minify: true,
+    format: "esm",
+    outdir: "assets",
+    write: false,
+    plugins: [...denoPlugins()],
+});
+
+await Deno.writeTextFile(
+    new URL("../assets/index.js", import.meta.url),
+    result.outputFiles[0].text,
+);
+await Deno.writeTextFile(
+    new URL("../assets/worker.js", import.meta.url),
+    result.outputFiles[1].text,
+);
+await stop();
