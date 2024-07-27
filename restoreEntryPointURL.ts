194b42526d3153e1e8fba0600fcaa7a959fc4e1f
takker99 <37929109+takker99@users.noreply.github.com>
2024-07-28 01:34:31 +0900

    fix: Failed to apply URLs to a template, due to percent encodings

--- restoreEntryPointURL.ts
+++ restoreEntryPointURL.ts
@@ -1,9 +1,9 @@
 import { extname } from "./deps/url.ts";
 
-export const restoreEntryPointURL = (path: string): URL => {
+export const restoreEntryPointURL = (path: string): string => {
   const [protocol, rest] = path.slice(1).split(":", 2);
 
   const tempURL = new URL(`${protocol}:/${rest}`);
   const pathname = tempURL.pathname.slice(0, -extname(tempURL).length);
-  return new URL(pathname, tempURL.origin);
+  return decodeURIComponent(new URL(pathname, tempURL.origin).href);
 };
