db76fad4ddb1a1f63adfb2e22fa5abd3a99a74bd
takker99 <37929109+takker99@users.noreply.github.com>
2024-07-31 09:10:44 +0900

    fix: Load NPM packages `exports` of which don't include `.`

--- deps/remoteLoader.ts
+++ deps/remoteLoader.ts
@@ -1,3 +1,3 @@
-export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.7.1/remoteLoader.ts";
-export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.7.1/robustFetch.ts";
-export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.7.1/deps/esbuild_deno_loader.ts";
+export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.7.2/remoteLoader.ts";
+export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.7.2/robustFetch.ts";
+export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.7.2/deps/esbuild_deno_loader.ts";
--- deps/toDataURL.ts
+++ deps/toDataURL.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.7.1/toDataURL.ts";
+export * from "https://raw.githubusercontent.com/takker99/ScrapJupyter/v1.7.2/toDataURL.ts";
